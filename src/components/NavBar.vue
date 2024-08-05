fe2bda7faf26a630c4d691c2e9894faecc08803c
cyclictriad <simonmwangi11416@gmail.com>
2024-08-05 15:03:13 +0300

    updated vercel backend

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,7 +24,7 @@
             :key="location"
             :value="location"
           >
-            {{ location }}
+            {{ location.name }}
           </option>
         </select>
       </div>
